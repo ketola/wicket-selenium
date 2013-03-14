d0f0148fe5d43c32acef2bc32a0b7c505d478bb8
ketola <sauli.ketola@mbnet.fi>
2013-03-14 21:55:37 +0200

    made IPanelLoader extend Serializable to avoid serialization warnings when used as anon. inner class

--- wicket-selenium-tester/src/main/java/ketola/wicket/selenium/tester/IPanelLoader.java
+++ wicket-selenium-tester/src/main/java/ketola/wicket/selenium/tester/IPanelLoader.java
@@ -1,8 +1,11 @@
 package ketola.wicket.selenium.tester;
 
+import java.io.Serializable;
+
 import org.apache.wicket.markup.html.panel.Panel;
 
 public interface IPanelLoader
+    extends Serializable
 {
     Panel getPanel( String id );
 }
